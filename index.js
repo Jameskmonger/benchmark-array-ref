5b5a48dbebaef1ce5694a8b973a84ac6c31904d9
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-04-15 13:52:13 +0100

    Finish benchmark suite

--- index.js
+++ index.js
@@ -3,3 +3,33 @@
 var Benchmark = require('benchmark');
 
 var suite = new Benchmark.Suite();
+
+var noop = function (val) { };
+
+suite.add('Reference array twice', function() {
+    var array = [1, 2, undefined, 4];
+
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] !== undefined) {
+            noop(array[i]);
+        }
+    }
+})
+.add('Reference array once', function() {
+    var array = [1, 2, undefined, 4];
+
+    for (var i = 0; i < array.length; i++) {
+        var arrayValue = array[i];
+
+        if (arrayValue !== undefined) {
+            noop(arrayValue);
+        }
+    }
+})
+.on('cycle', function(event) {
+    console.log(event.target);
+})
+.on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+})
+.run({ 'async': true });
